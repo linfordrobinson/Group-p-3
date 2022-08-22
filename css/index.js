abc18463f0666327018af9d7b58020e9f2f22d60
linfordrobinson <100168705+linfordrobinson@users.noreply.github.com>
2022-08-21 23:36:31 -0400

    H1 fixes
    H1 fixes

---
+++ css/index.js
@@ -0,0 +1,12 @@
+<script type='text/javascript'>
+$(function(){
+$('.input-group.date').datepicker({
+    orientation: "auto left",
+    forceParse: false,
+    autoclose: true,
+    todayHighlight: true,
+    toggleActive: true
+});
+});
+
+</script>
